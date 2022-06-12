ea09a62d0e151e31612e2a6fabb63a3184e0663f
Watcharin YangNgam <watcharin@opsta.co.th>
2022-06-12 18:24:33 +0700

    add new dynamic config

--- src/config.ts
+++ src/config.ts
@@ -8,4 +8,7 @@ import * as dotenv from "dotenv";
 require('dotenv').config();
 
 export const LoggerName = process.env.OPSTA_LOGGER_NAME;
+export const LogType = process.env.LOG_TYPE ?? "pretty";
+export const LogToFile = process.env.LOG_TO_FILE ?? "false";
+export const LogPath = process.env.LOG_PATH ?? "./";
 export const LoggerDelay = parseInt(process.env.OPSTA_LOGGER_DELAY ?? "10");
