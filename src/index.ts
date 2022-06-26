6659647a8af39cad7962d3102c7eded6e8c6a37a
Watcharin YangNgam <watcharin@opsta.co.th>
2022-06-26 18:02:38 +0700

    fix type bug

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,17 @@ import { LoggerDelay, LoggerName, LogToFile, LogPath, LogType } from "./config";
 import { appendFileSync } from "fs";
 
 // This line show --> how to include environment variable from OS!!
-console.log("This demo is use in %s", LoggerName)
 
 function logToTransport(logObject: ILogObject) {
   appendFileSync(LogPath + "tsdev.log", JSON.stringify(logObject) + "\n");
 }
 
-type LogType = 'pretty' | 'json' | 'hidden' | undefined;
-var mytype
-if (LogType == undefined) {
-  mytype = 'pretty' as LogType
-}
+type DefineLogType = 'pretty' | 'json' | 'hidden' | undefined;
+let mytype = LogType as DefineLogType;
 
 const log: Logger = new Logger({ name: LoggerName, type: mytype });
+
+log.info('This app running with', LoggerName)
 
 if (LogToFile.toLowerCase() == "true") {
   log.attachTransport(
@@ -41,39 +39,49 @@ function getRandomInt(max: number) {
 }
 
 const genLog = async () => {
+    log.info('Start writing logs.')
     while(true) {
         var ranNum = getRandomInt(LoggerDelay);
+        // console.log('random number is: %d',ranNum)
         await delay(ranNum * 1000)
         switch(ranNum) {
             case 0: {
+              console.log('In 0 case')
               log.silly("I am a silly log.");
               break;
             }
             case 1: {
+              console.log('In 1 case')
               log.trace("I am a trace log with a stack trace.");
               break;
             }
             case 2: {
+              console.log('In 2 case')
               log.debug("I am a debug log.");
               break;
             }
             case 3: {
+              console.log('In 3 case')
               log.fatal(new Error("I am a pretty Error with a stacktrace."));
               break;
             }
             case 4: {
+              console.log('In 4 case')
               log.warn("I am a warn log with a json object:", {foo: "bar"});
               break;
             }
             case 5: {
+              console.log('In 5 case')
               log.error("I am an error log.");
               break;
             }
             default: {
+              console.log('In default case')
               log.info("I am an info log.");
               break;
             }
         }
     }
 }
+log.info('Go to log generator.')
 genLog()
